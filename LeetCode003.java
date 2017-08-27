de8ca6b8562d5839dd07f996ebd635a0efd4bc64
leewbl <31314293+leewbl@users.noreply.github.com>
2017-08-27 16:38:49 +0800

    Update LeetCode003.java

--- LeetCode003.java
+++ LeetCode003.java
@@ -1,31 +1,37 @@
 package test;
 
-public class LeetCode003 {
-	class Solution {
-	    public int lengthOfLongestSubstring(String s) {
-	        int maxsum=0,sum=0;
-	        StringBuilder sb = new StringBuilder("");
-	        for(int i=0;i<s.length();i++){
-	            int k=-1;
-	            for(int j=0;j<sb.length();j++){
-	                if(sb.charAt(j)==s.charAt(j)){
-	                    k=j;break;
-	                }
-	            }
-	            if(k==-1){
-	                sum+=1;
-	                sb.append(s.charAt(i));
-	                if(sum>maxsum){
-	                    maxsum=sum;
-	                }
-	            }
-	            else{
-	                sb.delete(0,k+1); 
-	                sb.append(s.charAt(i));
-	                sum=sb.length();
-	            }
-	        }
-	        return maxsum;
-	    }
-	}
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int maxsum=0,sum=0;
+        char[] cy = s.toCharArray();
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<cy.length;i++){//time complexcity is O(N)
+            int k = inornot(sb,cy[i]);// since this step's time complexcity is O(N),so the total is O(N^2)?
+            if(k==-1){
+                sum+=1;
+                sb.append(cy[i]);
+                if(sum>maxsum){
+                    maxsum=sum;
+                }
+            }
+            else{
+                sb.delete(0,k+1); 
+                sb.append(cy[i]);
+                sum=sb.length();
+            }
+        }
+        return maxsum;
+    }
+//if StringBuilder has char c,return index
+//else return -1 
+//time complexcity is O(N)
+    public static int inornot(StringBuilder sbr,char c){
+        int j=-1;
+        for(int i=0;i<sbr.length();i++){
+            if(sbr.charAt(i)==c){
+                j=i;break;
+            }
+        }
+        return j;
+    }
 }
